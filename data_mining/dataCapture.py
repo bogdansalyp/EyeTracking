81d555237568e38e25e56f50774d0870ba999900
Bogdan Salyp <bogdansalyp@gmail.com>
2019-11-30 21:56:51 +0300

    Basic data mining

--- data_mining/dataCapture.py
+++ data_mining/dataCapture.py
@@ -3,33 +3,42 @@ import cv2
 import time
 import random
 import os
+# from videocaptureasync import VideoCaptureAsync
+
+BACKGROUND_COLOR = (111, 111, 111)
+CIRCLE_COLOR = (255, 255, 255)
+
+# 1280x720
 
 # Set directory to write to, Input name of participant, maybe some training or testing datasets (no need in "/" in the end)
-path = "/Users/olegserebrennikov/Documents/Prog/videoCapture/"
-dirName = input() + "/" + time.strftime("%d%b %H:%M")
+path = "dataDir/" + time.strftime("%d%b %H:%M:%S")
+window_name = "Hello"
+blankPath = "/Users/bogdansalyp/projects/EyeTracking/data_mining/blank.jpg"
 
 # Try to create a directory, if there is no success, program will not start capturing
 try:
-    os.makedirs(path + dirName)
-    os.chdir(path + dirName)
+    os.makedirs(path)
+    os.chdir(path)
 except OSError:
     exit()
 
 
 # Start timer with capture
 cnt = 1
-zeroSec = time.strftime("%s")
-firstSec = time.strftime("%s")
-spreadSec = 2 # x + 1 = Time between frame capture
+zeroTime = time.time()
+firstTime = time.time()
+spreadTime = 0 # x + 1 = Time between frame capture
 cap = cv2.VideoCapture(0)
+cap.set(3,320)
+cap.set(4,240)
 
 # Set the center of image
 xPos = 640
 yPos = 360
 
 # Set speed of circle
-xSpd = 20
-ySpd = 10
+xSpd = 4
+ySpd = 2
 
 # Set the start movement direction
 xFlag = "right" 
@@ -38,6 +47,8 @@ yFlag = "down"
 # Text settings
 font = cv2.FONT_HERSHEY_PLAIN
 textPos = (10,700)
+textPos2 = (1100,700)
+
 fontScale1 = 2
 fontScale2 = 1
 fontColorText = (255,255,255)
@@ -61,64 +72,66 @@ def xyChange(xPos, yPos, xSpd, ySpd):
 def posCheck(xPos, yPos, xFlag, yFlag):
 
     # Set the corners for specific text/geometry
-    if xPos == 1160:
+    if xPos == 1280:
         xFlag = "left"
     elif xPos == 0:
         xFlag = "right"
     if yPos == 720:
         yFlag = "up"
-    elif yPos == 20:
+    elif yPos == 0:
         yFlag = "down"
     return (xFlag, yFlag)
 
 time.sleep(3)
 
 with open("meta.txt", "w+") as f:
-    f.write("frame record delay: " + str(spreadSec) + "\n")
+    f.write("frame record delay: " + str(spreadTime) + "\n")
     f.write("X speed: " + str(xSpd) + "\n")
     f.write("Y speed: " + str(ySpd) + "\n")
 
+cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+circleImage = cv2.imread(blankPath)
+# cap.start()
 while True:
-    
-    # Capture frame-by-frame
-    ret, frame = cap.read(1,)
-
-    rgbImage = cv2.flip(cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA), 1)
-    newImage = rgbImage
 
     # Draw a circle
-    cv2.circle(rgbImage, (xPos, yPos), 10, (0, 0, 255), 10)
-    
-    # Draw a DVD label
-    #cv2.putText(rgbImage, dvdText, (xPos, yPos), font, fontScale1, fontColorDVD, lineType)
-    
-    # Refresh coordinates
-    xPos,  yPos  = xyChange(xPos, yPos, xSpd, ySpd)
-    xFlag, yFlag = posCheck(xPos, yPos, xFlag, yFlag)
+    for i in range(10):
+        cv2.circle(circleImage, (xPos, yPos), 10, BACKGROUND_COLOR, 10)
+        xPos,  yPos  = xyChange(xPos, yPos, xSpd, ySpd)
+        xFlag, yFlag = posCheck(xPos, yPos, xFlag, yFlag)
+        cv2.circle(circleImage, (xPos, yPos), 10, CIRCLE_COLOR, 10)
+        cv2.imshow(window_name, circleImage)
 
     # Write circle's position
-    cv2.putText(rgbImage, str(xPos) + " " + str(yPos), textPos, font, fontScale2, fontColorText, lineType)
-
+    # '''
+    # cv2.putText(circleImage, str(xPos) + " " + str(yPos), textPos, font, fontScale2, fontColorText, lineType)
+    # cv2.putText(circleImage, "frame: " + str(cnt), textPos2, font, fontScale2, fontColorText, lineType)
+    # '''
     # Display the resulting frame
-    cv2.imshow('frame', rgbImage)
+    #cv2.imshow('frame', circleImage)
 
     # Quit when "q" is pressed
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     
     # Lets take one frame every second and write it to our directory
-    if int(firstSec) - int(zeroSec) <= spreadSec:
-        firstSec = time.strftime("%s")
+    if firstTime - zeroTime <= spreadTime:
+        firstTime = time.time()
+        
     else:
+        # Capture frame
+        ret, frame = cap.read(1, )
         cnt = cnt + 1
         # Insert capture here
         pth = str(cnt) + " {" + str(xPos) + ":" + str(yPos) + "}" + ".jpg"
-        cv2.imwrite(pth, newImage)
-        zeroSec = firstSec
+        faceImage = cv2.flip(cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA), 1)
+        cv2.imwrite(pth, faceImage)
+        zeroTime = firstTime
 
 
 # When everything done, release the capture
+# cap.stop()
 cap.release()
 cv2.destroyAllWindows()
-
